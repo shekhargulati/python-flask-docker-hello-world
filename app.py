0205263fdc1e643f4067e48bc3ce7bf9b05305e5
Ayush Tiwari <36932180+GameLogist@users.noreply.github.com>
2019-10-29 17:56:10 +0530

    Made Port initialization dynamic (#6)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -8,4 +9,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
