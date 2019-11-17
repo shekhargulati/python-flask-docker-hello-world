557f531469af28a64e471b931b61b2ca5bcd2b3e
Marcus Ricardo Aguiar dos Santos <marcussantos@lnxcit020154.cit>
2019-11-17 14:09:42 -0500

    Port: 8080

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=True,host='0.0.0.0',port=port)
